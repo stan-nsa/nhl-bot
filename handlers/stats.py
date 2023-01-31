834b938faf57a5fa673827d0181d44771e951574
stan-nsa <stan.nsa@gmail.com>
2023-02-01 01:28:00 +1000

    nsa

--- handlers/stats.py
+++ handlers/stats.py
@@ -3,13 +3,17 @@ from nhl import nhl, stats
 
 
 async def command_stats(message: types.Message):
-    await message.answer(f"<b>Статистика:</b>\n{stats.get_stats_goalie_text(full=False)}", parse_mode="HTML")
+    await message.answer(f"<b>Статистика:</b>\n{stats.get_stats_text(full=False)}", parse_mode="HTML")
 
 
 async def command_stats_goalie(message: types.Message):
     await message.answer(f"<b>Статистика вратарей:</b>\n{stats.get_stats_goalie_text(full=False)}", parse_mode="HTML")
 
+async def command_stats_skater(message: types.Message):
+    await message.answer(f"<b>Статистика игроков:</b>\n{stats.get_stats_skater_text(full=False)}", parse_mode="HTML")
+
 
 def register_handlers_stats(dp : Dispatcher):
     dp.register_message_handler(command_stats, commands=['stats'])
     dp.register_message_handler(command_stats_goalie, commands=['stats_goalie'])
+    dp.register_message_handler(command_stats_skater, commands=['stats_skater'])
--- nhl/stats.py
+++ nhl/stats.py
@@ -1,6 +1,12 @@
+# Goalies
 #https://api.nhle.com/stats/rest/en/goalie/summary?isAggregate=false&isGame=false&sort=[{"property":"wins","direction":"DESC"},{"property":"savePct","direction":"DESC"}]&start=0&limit=50&factCayenneExp=gamesPlayed>=1&cayenneExp=gameTypeId=2 and seasonId=20222023
 #https://api.nhle.com/stats/rest/en/goalie/summary?isAggregate=false&isGame=false&sort=[{"property":"savePct","direction":"DESC"}]&start=0&limit=10&factCayenneExp=gamesPlayed>=15&cayenneExp=gameTypeId=2 and seasonId=20222023
 # desired stats: S/C(Shoots/Catches), GP, GS, W, L, OT (Overtime Losses), SA (Shots Against), Svs (Saves), GA (Goals Against), Sv% (Save %) GAA (Goals-Against-Avg), TOI, PIM
+
+# Skaters
+#https://api.nhle.com/stats/rest/en/skater/summary?isAggregate=false&isGame=false&sort=[{"property":"points","direction":"DESC"}]&start=0&limit=10&factCayenneExp=gamesPlayed>=1&cayenneExp=gameTypeId=2 and seasonId=20222023
+#https://api.nhle.com/stats/rest/en/skater/summary?isAggregate=false&isGame=false&sort=[{"property":"goals","direction":"DESC"}]&start=0&limit=10&factCayenneExp=gamesPlayed>=1&cayenneExp=gameTypeId=2 and seasonId=20222023
+#https://api.nhle.com/stats/rest/en/skater/summary?isAggregate=false&isGame=false&sort=[{"property":"assists","direction":"DESC"}]&start=0&limit=10&factCayenneExp=gamesPlayed>=1&cayenneExp=gameTypeId=2 and seasonId=20222023
 
 
 import requests
@@ -9,14 +15,28 @@ from nhl import nhl
 
 NHL_STATS_API_URL = "https://api.nhle.com/stats/rest/en"
 
+goalie_stats = {
+    'savePct': 'Sv%',
+    'goalsAgainstAverage': 'GGA',
+    'shutouts': 'SO',
+    'wins': 'W'
+}
+
+skater_stats = {
+    'points': 'P',
+    'goals': 'G',
+    'assists': 'A'
+}
+
 
 # Запрос к серверу для получения данных
-def get_request_nhl_stats_api(query_str : str):
+def get_request_nhl_stats_api(query_str: str):
     response = requests.get(NHL_STATS_API_URL + query_str, params={"Content-Type": "application/json"})
     return response.json()
 
-
-def get_stats_goalie(property : str, direction='DESC', limit=10):
+#-- Статистика вратарей ---------------------------------------------------------------------------
+# Получение данных вратарской статистики по указанному стат.показателю
+def get_stats_goalie_data_byProperty(property: str, direction='DESC', limit=10):
     gamesPlayed = 15
     gameTypeId = 2 # 2 = regular season, 3 = playoffs
     seasonId = nhl.get_season_current()['seasonId']
@@ -30,30 +50,119 @@ def get_stats_goalie(property : str, direction='DESC', limit=10):
 
     data = get_request_nhl_stats_api(query_str)
 
+    data['stata'] = property
+
     return data
 
 
-# Формирование теста для вывода турнирной таблицы с разбивкой по дивизионам
-def get_stats_goalie_text(full=False):
+# Формирование теста для вывода вратарской статистики
+def get_stats_goalie_text_fromData(data, full=False):
     #full = True
-    data = get_stats_goalie(property='savePct')
-
     txt = "<pre>"
 
     if (full):
-        txt += f"\n #|{'Player'.center(21, '_')}|Sv%\n"
+        txt += f"\n #|{'Goalie'.center(21, '_')}|{goalie_stats[data['stata']]}\n"
     else:
-        txt += f"\n #|{'Player'.center(21, '_')}|Sv%\n"
+        txt += f"\n #|{'Goalie'.center(21, '_')}|{goalie_stats[data['stata']]}\n"
 
     n = 0
     for player in data['data']:
         n += 1
         if (full):
-            #txt += f"{str(n).rjust(2, ' ')}|{player['goalieFullName'].ljust(20, ' ')}({player['teamAbbrevs']})|{round(player['savePct'], 3)}\n"
-            txt += f"{str(n).rjust(2, ' ')}|{player['goalieFullName'].ljust(21, ' ')}|{round(player['savePct'], 3)}\n"
+            #txt += f"{str(n).rjust(2, ' ')}|{player['goalieFullName'].ljust(20, ' ')}({player['teamAbbrevs']})|{round(player[data['stata']], 3)}\n"
+            txt += f"{str(n).rjust(2, ' ')}|{player['goalieFullName'].ljust(21, ' ')}|{round(player[data['stata']], 3)}\n"
         else:
-            # txt += f"{str(n).rjust(2, ' ')}|{player['goalieFullName'].ljust(20, ' ')}({player['teamAbbrevs']})|{round(player['savePct'], 3)}\n"
-            txt += f"{str(n).rjust(2, ' ')}|{player['goalieFullName'].ljust(21, ' ')}|{round(player['savePct'], 3)}\n"
+            # txt += f"{str(n).rjust(2, ' ')}|{player['goalieFullName'].ljust(20, ' ')}({player['teamAbbrevs']})|{round(player[data['stata']], 3)}\n"
+            txt += f"{str(n).rjust(2, ' ')}|{player['goalieFullName'].ljust(21, ' ')}|{round(player[data['stata']], 3)}\n"
 
     return txt + "</pre>"
+
+
+# Формирование теста для вывода вратарской статистики по указанному стат.показателю
+def get_stats_goalie_byProperty_text(property: str, direction='DESC', limit=10, full=False):
+    data = get_stats_goalie_data_byProperty(property, direction, limit)
+
+    text = get_stats_goalie_text_fromData(data, full)
+
+    return text
+
+
+# Формирование теста для вывода вратарских статистик
+def get_stats_goalie_text(full=False):
+    text = get_stats_goalie_byProperty_text(property='goalsAgainstAverage', direction='ASC', full=full)
+    text += get_stats_goalie_byProperty_text('savePct', full=full)
+    text += get_stats_goalie_byProperty_text('shutouts', full=full)
+    text += get_stats_goalie_byProperty_text('wins', full=full)
+
+    return text
+
+
+#-- Статистика полевых ----------------------------------------------------------------------------
+# Получение данных статистики полевых по указанному стат.показателю
+def get_stats_skater_data_byProperty(property: str, direction='DESC', limit=10):
+    gamesPlayed = 1
+    gameTypeId = 2 # 2 = regular season, 3 = playoffs
+    seasonId = nhl.get_season_current()['seasonId']
+
+    query_str = '/skater/summary?isAggregate=false&isGame=false'
+    query_str_sort = '&sort=[{"property":"' + property + '","direction":"' + direction + '"}]'
+    query_str_limit = f'&start=0&limit={limit}'
+    query_str_exp = f'&factCayenneExp=gamesPlayed>={gamesPlayed}&cayenneExp=gameTypeId={gameTypeId} and seasonId={seasonId}'
+
+    query_str += query_str_sort + query_str_limit + query_str_exp
+
+    data = get_request_nhl_stats_api(query_str)
+
+    data['stata'] = property
+
+    return data
+
+
+# Формирование теста для вывода статистики полевых
+def get_stats_skater_text_fromData(data, full=False):
+    #full = True
+    txt = "<pre>"
+
+    if (full):
+        txt += f"\n #|{'Skaters'.center(21, '_')}|{skater_stats[data['stata']]}\n"
+    else:
+        txt += f"\n #|{'Skaters'.center(21, '_')}|{skater_stats[data['stata']]}\n"
+
+    n = 0
+    for player in data['data']:
+        n += 1
+        if (full):
+            #txt += f"{str(n).rjust(2, ' ')}|{player['skaterFullName'].ljust(20, ' ')}({player['teamAbbrevs']})|{round(player[data['stata']], 3)}\n"
+            txt += f"{str(n).rjust(2, ' ')}|{player['skaterFullName'].ljust(21, ' ')}|{round(player[data['stata']], 3)}\n"
+        else:
+            # txt += f"{str(n).rjust(2, ' ')}|{player['skaterFullName'].ljust(20, ' ')}({player['teamAbbrevs']})|{round(player[data['stata']], 3)}\n"
+            txt += f"{str(n).rjust(2, ' ')}|{player['skaterFullName'].ljust(21, ' ')}|{round(player[data['stata']], 3)}\n"
+
+    return txt + "</pre>"
+
+
+# Формирование теста для вывода статистики полевых по указанному стат.показателю
+def get_stats_skater_byProperty_text(property: str, direction='DESC', limit=10, full=False):
+    data = get_stats_skater_data_byProperty(property, direction, limit)
+
+    text = get_stats_skater_text_fromData(data, full)
+
+    return text
+
+
+# Формирование теста для вывода статистик полевых
+def get_stats_skater_text(full=False):
+    text = get_stats_skater_byProperty_text(property='points', full=full)
+    text += get_stats_skater_byProperty_text('goals', full=full)
+    text += get_stats_skater_byProperty_text('assists', full=full)
+
+    return text
+
+
+# Формирование теста для вывода статистик
+def get_stats_text(full=False):
+    text = get_stats_skater_text(full=full)
+    text += get_stats_goalie_text(full=full)
+
+    return text
 
