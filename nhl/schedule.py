2a6ffa11d5be2a15c79105644df3af4fae43d24a
stan-nsa <stan.nsa@gmail.com>
2024-02-26 11:18:35 +1000

    nsa

--- nhl/schedule.py
+++ nhl/schedule.py
@@ -18,9 +18,9 @@ def get_schedule_dates_for_inlinemenu(day=None):
 
     data = get_schedule_day_data(day)
 
-    dates['previous'] = data['prevDate']
-    dates['next'] = data['nextDate']
-    dates['day'] = data['currentDate']
+    dates['previous'] = data.get('prevDate')
+    dates['next'] = data.get('nextDate')
+    dates['day'] = data.get('currentDate')
 
     return dates
 
@@ -31,14 +31,14 @@ def get_schedule_day_games_for_inlinemenu(day=None, data=None):
 
     games = []
 
-    for game in data['games']:
+    for game in data.get('games'):
 
         txt = get_schedule_game_text(game, inlinemenu=True)
 
         # Play-off series Summary
-        #txt += f" ({game['seriesSummary']['gameLabel'].replace('ame ', '#')} {game['seriesSummary']['seriesStatusShort']})" if (game['gameType'] == "P") else ""
+        #txt += f" ({game.get('seriesSummary').get('gameLabel').replace('ame ', '#')} {game.get('seriesSummary').get('seriesStatusShort')})" if (game.get('gameType') == "P") else ""
 
-        games.append({'id': game['id'], 'text': txt})
+        games.append({'id': game.get('id'), 'text': txt})
 
     return games
 
@@ -65,30 +65,30 @@ def get_schedule_day_text(day=None, data=None, hideScore=False):
 # Формирование теста для вывода расписания
 def get_schedule_days_text(data, hideScore=False):
 
-    txt = f"\n{nhl.ico['schedule']} <b>{data['currentDate']}:</b>\n"
+    txt = f"\n{nhl.ico.get('schedule')} <b>{data.get('currentDate')}:</b>\n"
 
     header_live = ''
     header_scheduled = ''
     header_finished = ''
-    for game in data['games']:
+    for game in data.get('games'):
         # Scheduled
-        if game['gameState'] in nhl.gameState['scheduled']:
+        if game.get('gameState') in nhl.gameState.get('scheduled'):
             if (header_scheduled == ''): # Заголовок таблицы расписания
                 header_scheduled = \
-                    f"\n{nhl.ico['time']} <b>Scheduled:</b>\n" \
-                    f"<code>___{nhl.ico['vs']}___{nhl.ico['time']}_EST_|_MSK_|_KHV_</code>\n"
+                    f"\n{nhl.ico.get('time')} <b>Scheduled:</b>\n" \
+                    f"<code>___{nhl.ico.get('vs')}___{nhl.ico.get('time')}_EST_|_MSK_|_KHV_</code>\n"
                 txt += header_scheduled
 
         # Live
-        elif game['gameState'] in nhl.gameState['live']:
+        elif game.get('gameState') in nhl.gameState.get('live'):
             if (header_live == ''):
-                header_live = f"\n{nhl.ico['live']} <b>Live:</b>\n"
+                header_live = f"\n{nhl.ico.get('live')} <b>Live:</b>\n"
                 txt += header_live
 
         # Final
-        elif game['gameState'] in nhl.gameState['final']:
+        elif game.get('gameState') in nhl.gameState.get('final'):
             if (header_finished == ''):
-                header_finished = f"\n{nhl.ico['finished']} <b>Finished:</b>\n"
+                header_finished = f"\n{nhl.ico.get('finished')} <b>Finished:</b>\n"
                 txt += header_finished
 
         txt += get_schedule_game_text(game, hideScore=hideScore) + "\n"
@@ -100,33 +100,34 @@ def get_schedule_days_text(data, hideScore=False):
 def get_schedule_game_text(game, hideScore=False, inlinemenu=False):
 
     # Scheduled
-    if game['gameState'] in nhl.gameState['scheduled']:
-        txt = f"{game['awayTeam']['abbrev']}{nhl.ico['vs']}{game['homeTeam']['abbrev']}" \
-               f"{nhl.ico['time']}{get_game_time_tz_text(game['startTimeUTC'], inlinemenu=inlinemenu)}"
+    if game.get('gameState') in nhl.gameState.get('scheduled'):
+        txt = f"{game.get('awayTeam').get('abbrev')}{nhl.ico.get('vs')}{game.get('homeTeam').get('abbrev')}" \
+              f"{nhl.ico.get('time')}{get_game_time_tz_text(game.get('startTimeUTC'), inlinemenu=inlinemenu)}"
         txt = txt if (inlinemenu) else f"<code>{txt}</code>"
 
     # Live
-    elif game['gameState'] in nhl.gameState['live']:
-        currentPeriod = game['periodDescriptor']['periodType'] if game['periodDescriptor']['number'] > 3 else nhl.gamePeriods[game['periodDescriptor']['number']]
-        txt = f"{get_game_teams_score_text(game, hideScore=hideScore, inlinemenu=inlinemenu)} - " \
-              f"{nhl.ico['live']} {currentPeriod}/" \
-              f"{'END' if (game['clock']['inIntermission']) else game['clock']['timeRemaining']}"
+    elif game.get('gameState') in nhl.gameState.get('live'):
+        currentPeriod = game.get('periodDescriptor').get('periodType') if game.get('periodDescriptor').get('number') > 3 else nhl.gamePeriods[game.get('periodDescriptor').get('number')]
+        teams_score_text = get_game_teams_score_text(game, hideScore=hideScore, inlinemenu=inlinemenu)
+        txt = f"{teams_score_text} - {nhl.ico.get('live')} {currentPeriod}"
+        if game.get('clock'):
+            txt += f"/{'END' if (game.get('clock').get('inIntermission')) else game.get('clock').get('timeRemaining')}"
 
     # Final
-    elif game['gameState'] in nhl.gameState['final']:
+    elif game.get('gameState') in nhl.gameState.get('final'):
         teams_score_text = get_game_teams_score_text(game, hideScore=hideScore, inlinemenu=inlinemenu)
         txt = f"{teams_score_text} - " \
-               f"{nhl.ico['finished']} {'' if game['periodDescriptor']['number'] == 3 else game['periodDescriptor']['periodType']}"
+              f"{nhl.ico.get('finished')} {'' if game.get('periodDescriptor').get('number') == 3 else game.get('periodDescriptor').get('periodType')}"
 
     # TBD/Postponed
-    elif game['gameState'] in nhl.gameState['tbd']:  # 8 - Scheduled (Time TBD); 9 - Postponed
-        txt = f"{game['awayTeam']['abbrev']}{nhl.ico['vs']}{game['homeTeam']['abbrev']}" \
-               f"{nhl.ico['tbd']} {game['gameState']}"
+    elif game.get('gameState') in nhl.gameState.get('tbd'):  # 8 - Scheduled (Time TBD); 9 - Postponed
+        txt = f"{game.get('awayTeam').get('abbrev')}{nhl.ico.get('vs')}{game.get('homeTeam').get('abbrev')}" \
+              f"{nhl.ico.get('tbd')} {game.get('gameState')}"
         txt = txt if (inlinemenu) else f"<code>{txt}</code>"
 
     # Other
     else:
-        txt = f"{game['awayTeam']['abbrev']}{nhl.ico['vs']}{game['homeTeam']['abbrev']}"
+        txt = f"{game.get('awayTeam').get('abbrev')}{nhl.ico.get('vs')}{game.get('homeTeam').get('abbrev')}"
         txt = txt if (inlinemenu) else f"<code>{txt}</code>"
 
     return txt
@@ -134,13 +135,13 @@ def get_schedule_game_text(game, hideScore=False, inlinemenu=False):
 
 def get_game_teams_score_text(game, hideScore=False, inlinemenu=False):
 
-    away_team = game['awayTeam']['abbrev'] if (inlinemenu) else f"<code>{game['awayTeam']['abbrev']}</code>"
-    away_team_score = game['awayTeam']['score']
+    away_team = game.get('awayTeam').get('abbrev') if (inlinemenu) else f"<code>{game.get('awayTeam').get('abbrev')}</code>"
+    away_team_score = game.get('awayTeam').get('score')
 
-    home_team = game['homeTeam']['abbrev'] if (inlinemenu) else f"<code>{game['homeTeam']['abbrev']}</code>"
-    home_team_score = game['homeTeam']['score']
+    home_team = game.get('homeTeam').get('abbrev') if (inlinemenu) else f"<code>{game.get('homeTeam').get('abbrev')}</code>"
+    home_team_score = game.get('homeTeam').get('score')
 
-    game_teams_score = f"{away_team} {get_game_team_score_text(away_team_score, hideScore=hideScore)}{nhl.ico['vs']}{get_game_team_score_text(home_team_score, hideScore=hideScore)} {home_team}"
+    game_teams_score = f"{away_team} {get_game_team_score_text(away_team_score, hideScore=hideScore)}{nhl.ico.get('vs')}{get_game_team_score_text(home_team_score, hideScore=hideScore)} {home_team}"
 
     return game_teams_score
 
@@ -198,11 +199,11 @@ def get_game_time_tz_text(dt_str, withDate=False, withTZ=False, inlinemenu=False
 
     dt = datetime.strptime(dt_str, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
 
-    dtEST = dt.astimezone(nhl.tz['EST'])
-    dtMSK = dt.astimezone(nhl.tz['MSK'])
-    dtKHV = dt.astimezone(nhl.tz['KHV'])
+    dtEST = dt.astimezone(nhl.tz.get('EST'))
+    dtMSK = dt.astimezone(nhl.tz.get('MSK'))
+    dtKHV = dt.astimezone(nhl.tz.get('KHV'))
 
-    str = f"{dtEST.date().isoformat()} {nhl.ico['time']} " if (withDate) else ''
+    str = f"{dtEST.date().isoformat()} {nhl.ico.get('time')} " if (withDate) else ''
 
     ft = "%H:%M %Z" if (withTZ or inlinemenu) else "%H:%M"
     str += f"{dtEST.strftime(ft)}|{dtMSK.strftime(ft)}|{dtKHV.strftime(ft).replace('+10', 'KHV')}".upper() #lower()
@@ -214,10 +215,10 @@ def get_game_time_tz_text(dt_str, withDate=False, withTZ=False, inlinemenu=False
 
 # Получение корректной даты, если получены пустые данные
 def get_correct_date_from_data(data, as_str=True):
-    if (data['totalItems'] > 0):
-        day = data['dates'][0]['date'] if (as_str) else date.fromisoformat(data['dates'][0]['date'])
+    if (data.get('totalItems') > 0):
+        day = data.get('dates').get(0).get('date') if (as_str) else date.fromisoformat(data.get('dates').get(0).get('date'))
     else:
-        day = datetime.strptime(data['metaData']['timeStamp'], "%Y%m%d_%H%M%S").replace(tzinfo=timezone.utc).astimezone(nhl.tz['EST'])
+        day = datetime.strptime(data.get('metaData').get('timeStamp'), "%Y%m%d_%H%M%S").replace(tzinfo=timezone.utc).astimezone(nhl.tz.get('EST'))
         day = day.date().isoformat() if (as_str) else day
 
     return day
