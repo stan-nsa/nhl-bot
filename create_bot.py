73db4de192426f7574a8a82b4022e2f31ba4a73b
NSA <stan.nsa@gmail.cm>
2024-03-06 00:12:52 +1000

    nsa

--- create_bot.py
+++ create_bot.py
@@ -7,8 +7,8 @@ with open('config.json') as f:
     config = json.load(f)
 #----
 
-api_token_key = 'API_TOKEN'
-#api_token_key = 'API_TOKEN_TEST'
+#api_token_key = 'API_TOKEN'
+api_token_key = 'API_TOKEN_TEST'
 
 bot = Bot(token=config[api_token_key])
 
